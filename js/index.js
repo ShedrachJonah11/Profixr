89e2442914708a36706199879238fe3aaf0ee21e
Prosper-ekube <prosperekube@gmail.com>
2023-07-24 02:19:01 +0100

    fix: use proper file paths for all pages

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const navBar = document.getElementById("navBar");
-
-function showMobileNav() {
-    if (navBar.className === "nav-bar") {
-        navBar.className += " mobile";
-    } else {
-        navBar.className = "nav-bar";
-    }
-}
